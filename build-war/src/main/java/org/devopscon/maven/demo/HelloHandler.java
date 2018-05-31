c97c77c01891f48a6454a0235d7dba022490d543
beernbytes <beernbytes@gmail.com>
2018-05-31 14:33:35 +0000

    changed

--- build-war/src/main/java/org/devopscon/maven/demo/HelloHandler.java
+++ build-war/src/main/java/org/devopscon/maven/demo/HelloHandler.java
@@ -3,7 +3,7 @@ package org.devopscon.maven.demo;
 public class HelloHandler {
 
 	public String sayHello() {
-		String hello = "Hello DevOpsCon, What do you think about this WAR file?";
+		String hello = "Hello DevOpsCon my first change, What do you think about this WAR file?";
 		return hello;
 	}
 
